6b7186a9601bb1b7eefe16384076bfe97ccd9e96
Vitor <93414564+Asugamy@users.noreply.github.com>
2024-04-11 13:19:56 -0300

    Update todo.py

--- todo.py
+++ todo.py
@@ -1,221 +1,124 @@
-import flet as ft
+from flet import *
 import database
 
-def main(page: ft.Page):
-    page.theme_mode = ft.ThemeMode.LIGHT
-    page.title = "GEM Morrinhos"
+class Card:
 
-    # Menu Lateral
-    def login(e):
-        attempt = database.authentication(user.value, password.value)
-        if attempt:
-            return page.go("/todo")
-        else:
+    def __init__(self, id):
+            self.id_card = id
 
-            page.snack_bar = ft.SnackBar(
-                ft.Text(
-                    "Usuário/Senha inconreto(s)! Tente novamente.",
-                    text_align=ft.alignment.center,
-                    weight=ft.FontWeight.W_600
+    def card_a_fazer(self, tipo, instrutor, mensagem):
+        truco = ft.Card(
+            surface_tint_color=ft.colors.TRANSPARENT,
+            content=ft.Container(
+                content=ft.Column(
+                    [
+                        ft.ListTile(
+                            title=ft.Text(f"{tipo} - {instrutor}"),
+                            subtitle=ft.Text(f'{mensagem}'),
+                        ),
+                        ft.Row(
+                            [ft.TextButton("Estudei", on_click=lambda _: estudei(self.id_card)),
+                             ft.TextButton("Abrir")],
+                            alignment=ft.MainAxisAlignment.SPACE_AROUND,
+                        ),
+                    ]
                 ),
-                bgcolor=ft.colors.RED_ACCENT_400,
-
-            )
-            page.snack_bar.open = True
-            page.update()
-
-    form_task_aluno = ft.Dropdown(
-        label='Candidato',
-        hint_text='Candidato',
-        options=[
-            ft.dropdown.Option('João Marcelo'),
-            ft.dropdown.Option('Marcos'),
-            ft.dropdown.Option('Thiago'),
-        ]
-    )
-
-    form_task_tipo = ft.Dropdown(
-        label='Assunto',
-        hint_text='Assunto',
-        options=[
-            ft.dropdown.Option('MSA'),
-            ft.dropdown.Option('MTS'),
-            ft.dropdown.Option('Método Instrumento'),
-            ft.dropdown.Option('Hinos'),
-        ]
-    )
-
-    form_task_mensagem = ft.TextField(label='Digite a tarefa', multiline=True)
-
-    # def adicionar_task(e):
-    #     database.add_tasks('Vitor', form_task_mensagem.value, 'aFazer', form_task_tipo.value, form_task_aluno.value)
-
-    # Cards
-    # cards = database.get_cards('João Marcelo')
-
-    # def a_fazer():
-    #     cards_a_fazer = []
-    #     for card in cards['aFazer']:
-    #         cards_a_fazer.append(
-    #             ft.Card(
-    #                 surface_tint_color=ft.colors.TRANSPARENT,
-    #                 content=ft.Container(
-    #                     content=ft.Column(
-    #                         [
-    #                             ft.ListTile(
-    #                                 title=ft.Text(f"{card['tipo']} - {card['instrutor']}"),
-    #                                 subtitle=ft.Text(f'{card["mensagem"]}'),
-    #                             ),
-    #                             ft.Row(
-    #                                 [ft.TextButton("Estudei", on_click=lambda: estudei(card['id'])),
-    #                                  ft.TextButton("Abrir")],
-    #                                 alignment=ft.MainAxisAlignment.SPACE_AROUND,
-    #                             ),
-    #                             ft.TextField(value=card['id'], visible=False),
-    #                         ]
-    #                     ),
-    #                     width=1800,
-    #                     padding=10,
-    #                     bgcolor=ft.colors.WHITE,
-    #                     border=ft.border.all(1, '#9AB9FF'),
-    #                     border_radius=5,
-    #                     margin=ft.margin.only(0, 30, 0, 0)
-    #                 ),
-    #             )
-    #         )
-    #     return ft.ListView(controls=cards_a_fazer)
-
-    # def estudei(id):
-    #     for card in cards['aFazer']:
-    #         if card['id'] == id :
-    #             card['status'] = 'concluido'
-    #             page.update()
-
-    def page_add(e):
-        return page.go("/add")
-
-    page.add(
-        #Título
-        ft.Container(
-            ft.Text(
-                'Tarefas',
-                size=20,
-                weight=ft.FontWeight.W_600,
-                color='#407BFF'
+                width=1800,
+                padding=10,
+                bgcolor=ft.colors.WHITE,
+                border=ft.border.all(1, '#9AB9FF'),
+                border_radius=5,
+                margin=ft.margin.only(0, 30, 0, 0)
             ),
-            bgcolor=ft.colors.WHITE,
-            margin=ft.margin.only(0,45,0,25),
-        ),
+        )
+        return truco
+
+    def card_concluido(self, tipo, instrutor, mensagem):
+        truco = ft.Card(
+            surface_tint_color=ft.colors.TRANSPARENT,
+            content=ft.Container(
+                content=ft.Column(
+                    [
+                        ft.ListTile(
+                            title=ft.Text(f"{tipo} - {instrutor}"),
+                            subtitle=ft.Text(f'{mensagem}'),
+                        ),
+                        ft.Row(
+                            [ft.TextButton("Cancelar", on_click=lambda _: cancela_estudei(self.id_card))],
+                            alignment=ft.MainAxisAlignment.SPACE_AROUND,
+                        ),
+                    ]
+                ),
+                width=1800,
+                padding=10,
+                bgcolor=ft.colors.WHITE,
+                border=ft.border.all(1, '#9AB9FF'),
+                border_radius=5,
+                margin=ft.margin.only(0, 30, 0, 0)
+            ),
+        )
+        return truco
+
+class ToDo(UserControl):
+
+    cards = database.get_cards('João Marcelo')
+
+    def __init__(self):
+        super().__init__()
+
+        self.appSpace = Column()
+
+    def build(self):
+
+        '''ESPAÇOS'''
+        self.tasks = Column()
+
+        '''ELMENTOS'''
 
         #Tabs
-        ft.Row(
-            [
-                ft.Tabs(
-                    selected_index=1,
-                    animation_duration=300,
-                    tabs=[
-                        ft.Tab(
-                            text="A FAZER",
-                            content=ft.Column(
-                                [
-                                    ft.Card(
-                                        surface_tint_color=ft.colors.WHITE,
-                                        content=ft.Container(
-                                            content=ft.Column(
-                                                [
-                                                    ft.ListTile(
-                                                        title=ft.Text("Método - Almir"),
-                                                        subtitle=ft.Text(
-                                                            "Estudar novamente a lição 16 da página 14, lembrando de pontuar"
-                                                        ),
-                                                    ),
-                                                    ft.Row(
-                                                        [ft.TextButton("Estudei"), ft.TextButton("Abrir")],
-                                                        alignment=ft.MainAxisAlignment.SPACE_AROUND,
-                                                    ),
-                                                ]
-                                            ),
-                                            width=1800,
-                                            padding=10,
-                                            bgcolor=ft.colors.WHITE,
-                                            border=ft.border.all(1, '#9AB9FF'),
-                                            border_radius=5,
-                                            margin=ft.margin.only(0,30,0,0)
-                                        ),
-                                    ),
-                                    ft.Card(
-                                        surface_tint_color=ft.colors.WHITE,
-                                        content=ft.Container(
-                                            content=ft.Column(
-                                                [
-                                                    ft.ListTile(
-                                                        title=ft.Text("Método - Almir"),
-                                                        subtitle=ft.Text(
-                                                            "Estudar novamente a lição 16 da página 14, lembrando de pontuar"
-                                                        ),
-                                                    ),
-                                                    ft.Row(
-                                                        [ft.TextButton("Estudei"), ft.TextButton("Abrir")],
-                                                        alignment=ft.MainAxisAlignment.SPACE_AROUND,
-                                                    ),
-                                                ]
-                                            ),
-                                            width=1800,
-                                            padding=10,
-                                            bgcolor=ft.colors.WHITE,
-                                            border=ft.border.all(1, '#9AB9FF'),
-                                            border_radius=5,
-                                            margin=ft.margin.only(0,15,0,0)
-                                        ),
-                                    ),
-                                    ft.Card(
-                                        surface_tint_color=ft.colors.WHITE,
-                                        content=ft.Container(
-                                            content=ft.Column(
-                                                [
-                                                    ft.ListTile(
-                                                        title=ft.Text("Método - Almir"),
-                                                        subtitle=ft.Text(
-                                                            "Estudar novamente a lição 16 da página 14, lembrando de pontuar"
-                                                        ),
-                                                    ),
-                                                    ft.Row(
-                                                        [ft.TextButton("Estudei"), ft.TextButton("Abrir")],
-                                                        alignment=ft.MainAxisAlignment.SPACE_AROUND,
-                                                    ),
-                                                ]
-                                            ),
-                                            width=1800,
-                                            padding=10,
-                                            bgcolor=ft.colors.WHITE,
-                                            border=ft.border.all(1, '#9AB9FF'),
-                                            border_radius=5,
-                                            margin=ft.margin.only(0,15,0,0),
-                                        ),
-                                    ),
-                                ],
-                                scroll=ft.ScrollMode.ALWAYS
-                            )
-                        ),
-                        ft.Tab(
-                            text='CONCLUÍDAS',
-                            content=ft.Text("This is Tab 2"),
-                        ),
-                        ft.Tab(
-                            text="CORRIGIDAS",
-                            content=ft.Text("This is Tab 3"),
-                        ),
-                    ],
-                    expand=1,
-                    tab_alignment=ft.MainAxisAlignment.CENTER,
-                    divider_color=ft.colors.WHITE
+        self.filter = Tabs(
+                scrollable=False,
+                selected_index=0,
+                # on_change=self.tabs_changed,
+                tabs=[Tab(text="all"), Tab(text="active"), Tab(text="completed")],
+            )
+
+        def a_fazer(self):
+            cards_a_fazer = []
+            for card in cards['aFazer']:
+                task = Card(card['id'])
+                cards_a_fazer.append(
+                    task.card_a_fazer(card['tipo'], card['instrutor'], card['mensagem'])
                 )
-            ],
-            expand=True,
+            return ft.ListView(controls=cards_a_fazer)
 
-        ),
+        return Column([
+            Row([
+                Text(
+                    'Tarefa',
+                    size=20,
+                    weight=FontWeight.W_600,
+                    color='#407BFF'
+                ),
 
-        ft.FloatingActionButton(icon=ft.icons.ADD)
+            ]),
+            Column([
+                self.filter,
+                self.tasks,
+
+            ])
+        ])
+
+
+def main(page:Page):
+
+    page.theme_mode = ThemeMode.LIGHT
+    page.title = "GEM Morrinho"
+
+    myApp = ToDo()
+
+    page.add(
+        myApp
     )
 
-ft.app(main)
+app(target=main)
